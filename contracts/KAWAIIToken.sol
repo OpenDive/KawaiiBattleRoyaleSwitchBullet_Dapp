7c2ad93fa4fca46b9be90e63d8c0495399fbdbff
kPatch <irvsteve@gmail.com>
2021-01-16 04:40:15 -0500

    Update KAWAII token with roles

--- contracts/KAWAIIToken.sol
+++ contracts/KAWAIIToken.sol
@@ -6,8 +6,12 @@ contract KAWAIIToken is ERC20PresetMinterPauser {
     // constructor(uint256 initialSupply) public ERC20("Kawaii", "KAWAII") {
     //     _mint(msg.sender, initialSupply);
     // }
+    bytes32 public constant MINTER_ROLE = keccak256("MY_ROLE");
 
-    constructor() public ERC20PresetMinterPauser("Kawaii", "KAWAII") {
-        _mint(address(this), 3000000000);   // 3,000,000,000 - 3 Billion
+    constructor(address _governance) public ERC20PresetMinterPauser("Kawaii", "KAWAII") {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(MINTER_ROLE, _governance);
+        _setupRole(PAUSER_ROLE, msg.sender);
+        _mint(_governance, 3000000000);   // 3,000,000,000 - 3 Billion
     }
 }
