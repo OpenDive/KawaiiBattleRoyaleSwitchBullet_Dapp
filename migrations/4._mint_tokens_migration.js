e9366790bded744d1bdc1827c4d63dc1520a8a7b
kPatch <irvsteve@gmail.com>
2021-01-16 04:58:43 -0500

    Update migration mint format

--- migrations/4._mint_tokens_migration.js
+++ migrations/4._mint_tokens_migration.js
@@ -15,12 +15,14 @@ module.exports = function (deployer, _network, accounts) {
   const eighteen    = 100000000000000000
   const governance  = accounts[0];
   const address     = Marketplace.address;
+  // const address     = "0x3dbd676C1849bC951fDaeF766B20cda3d35060EE";
   const ids         = [ SUPER_BOOTS, TIMEWARP_CAPE, JETPACK, THORS_HAMMER, SWORD, SHIELD, LOVE_GUN ];
   // const amounts     = [ String(2 * eighteen), String(1 * eighteen), String(1000 * eighteen), String(10 * eighteen), String(5 * eighteen), String(4 * eighteen), String(200 * eighteen)];
   const amounts     = ["2", "1", "1000", "10", "5", "4", "200"];
   const data        = "0x12";
 
   GameItems.deployed().then(instance => {
-    instance.mintBatch(address, ids, amounts, data);
+    // instance.mintBatchItems(address, ids, amounts, data);
+    instance.mint(address, 0, 10, data);
   });
 };
