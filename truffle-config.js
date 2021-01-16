87e91a6b059f0e8c998a678a3a56331949e3a413
kPatch <irvsteve@gmail.com>
2021-01-16 04:38:36 -0500

    Update Harmony truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,25 @@
+require("dotenv").config();
+const { TruffleProvider } = require("@harmony-js/core");
+
+//Local
+// const local_mnemonic = process.env.LOCAL_MNEMONIC;
+// const local_private_key = process.env.LOCAL_PRIVATE_KEY;
+// const local_url = process.env.LOCAL_0_URL;
+
+//Testnet
+const testnet_mnemonic = process.env.TESTNET_MNEMONIC;
+const testnet_private_key = process.env.TESTNET_PRIVATE_KEY;
+const testnet_url = process.env.TESTNET_0_URL;
+
+//Mainnet
+const mainnet_mnemonic = process.env.MAINNET_MNEMONIC;
+const mainnet_private_key = process.env.MAINNET_PRIVATE_KEY;
+const mainnet_url = process.env.MAINNET_0_URL;
+
+//GAS - Currently using same GAS accross all environments
+gasLimit = process.env.GAS_LIMIT;
+gasPrice = process.env.GAS_PRICE;
+
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -36,6 +58,34 @@ module.exports = {
    */
 
   networks: {
+    testnet0Harmony: {
+      network_id: "2", // Any network (default: none)
+      provider: () => {
+        const truffleProvider = new TruffleProvider(
+          testnet_url,
+          { memonic: testnet_mnemonic },
+          { shardID: 0, chainId: 2 },
+          { gasLimit: gasLimit, gasPrice: gasPrice }
+        );
+        const newAcc = truffleProvider.addByPrivateKey(testnet_private_key);
+        truffleProvider.setSigner(newAcc);
+        return truffleProvider;
+      },
+    },
+    mainnet0Harmony: {
+      network_id: "1", // Any network (default: none)
+      provider: () => {
+        const truffleProvider = new TruffleProvider(
+          mainnet_url,
+          { memonic: mainnet_mnemonic },
+          { shardID: 0, chainId: 1 },
+          { gasLimit: gasLimit, gasPrice: gasPrice }
+        );
+        const newAcc = truffleProvider.addByPrivateKey(mainnet_private_key);
+        truffleProvider.setSigner(newAcc);
+        return truffleProvider;
+      },
+    },
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
