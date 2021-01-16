16b165eb7716f9f7c50fcfb4b75a683393316c37
kPatch <irvsteve@gmail.com>
2021-01-16 02:37:19 -0500

    Update KBR game contract

--- contracts/KBRGame.sol
+++ contracts/KBRGame.sol
@@ -3,24 +3,25 @@ pragma solidity ^0.6.0;
 import "@openzeppelin/contracts/GSN/Context.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
 
 // Hold KAWAII Tokens
 // In charge of managing game
-contract KBRGame {
+contract KBRGame is Ownable {
     address oracle;
     IERC20 busd;
     IERC20 link;
-    IERC20 gameTokens;
+    ERC20PresetMinterPauser kawaii;
 
-    uint defaultStake;
-    uint minStakeDays;
+    uint public defaultStake;
+    uint public minStakeDays;
 
     struct Player {
         uint    timeJoined;
-        uint    amountStake;
+        uint    amountStaked;
     }
 
-    mapping(address => Players) public players;
+    mapping(address => Player) public players;
 
     event NewUserStakedAccess(address playerAddress);
     event UserWithdrewStake(address playerAddress);
@@ -30,50 +31,48 @@ contract KBRGame {
         address _oracle, 
         address _busd, 
         address _link,
-        address _kawaii ) {
+        address _kawaii ) public {
 
         oracle  = _oracle;
         busd    = IERC20(_busd);
         link    = IERC20(_link); 
-        kawaii  = IERC20(_kawaii);
+        kawaii  = ERC20PresetMinterPauser(_kawaii);
 
         defaultStake = 30 * 10**18; // 30 BUSD
         minStakeDays = 30 days;
     }
 
     /// Stake to get acess to the game
-    /// @param x the new value to store
     /// @dev the minimum required is updated based on the phase of the game relase
-    function stakeToPlayBUSD() payable {
+    function stakeToPlayBUSD() public payable {
         uint256 allowance = busd.allowance(msg.sender, oracle);
         require(allowance >= defaultStake, "Check the token allowance");
         busd.transferFrom(_msgSender(), oracle, defaultStake);
 
-        Player player = Player({ timeJoined: now, amountStaked: allowance });
+        Player memory player = Player({ timeJoined: now, amountStaked: allowance });
         players[msg.sender] = player;
-        kawaii.mint(2000 * 10**18); // Mint KAWAII tokens for staking
+        kawaii.mint(msg.sender, 2000 * 10**18); // Mint KAWAII tokens for staking
         emit NewUserStakedAccess(msg.sender);
     }
 
     /// Need to check equivalent amount of linke to defaultStake
-    function stakeToPlayLINK() payable {
+    function stakeToPlayLINK() public payable {
         uint256 allowance = link.allowance(msg.sender, oracle);
         require(allowance >= defaultStake, "Check the token allowance");
         link.transferFrom(_msgSender(), oracle, defaultStake);
 
-        Player player = Player({ timeJoined: now, amountStaked: allowance });
+        Player memory player = Player({ timeJoined: now, amountStaked: allowance });
         players[msg.sender] = player;
-        kawaii.mint(2000 * 10**18); // Mint KAWAII tokens for staking
+        kawaii.mint(msg.sender, 2000 * 10**18); // Mint KAWAII tokens for staking
         emit NewUserStakedAccess(msg.sender);
     }
 
     ///
-    /// @param x t
     /// @dev 
-    function withdrawStake() {
-        Player memory player = player[msg.sender];
+    function withdrawStake() public {
+        Player memory player = players[msg.sender];
         require(player.timeJoined >= player.timeJoined + minStakeDays, "Cannot withdraw yet");
-        busd.transfer(msg.sender, player.amountStake);
+        busd.transfer(msg.sender, player.amountStaked);
 
         emit UserWithdrewStake(msg.sender);
     }
