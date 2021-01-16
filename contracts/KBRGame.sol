4c5aa3363fdc45be38288fe5222888e20573ad7d
kPatch <irvsteve@gmail.com>
2021-01-15 22:48:26 -0500

    Allow withdrawals

--- contracts/KBRGame.sol
+++ contracts/KBRGame.sol
@@ -13,15 +13,17 @@ contract KBRGame {
     IERC20 gameTokens;
 
     uint defaultStake;
+    uint minStakeDays;
 
     struct Player {
-        address player;
+        uint    timeJoined;
         uint    amountStake;
     }
 
-    mapping(address => bool) public changeClaimed;
+    mapping(address => Players) public players;
 
     event NewUserStakedAccess(address playerAddress);
+    event UserWithdrewStake(address playerAddress);
     event DefaultStakeUpdated(uint newAmount);
 
     constructor(
@@ -36,6 +38,7 @@ contract KBRGame {
         kawaii  = IERC20(_kawaii);
 
         defaultStake = 30 * 10**18; // 30 BUSD
+        minStakeDays = 30 days;
     }
 
     /// Stake to get acess to the game
@@ -44,11 +47,29 @@ contract KBRGame {
     /// @dev 
     function stakeToPlay() payable {
         uint256 allowance = busd.allowance(msg.sender, oracle);
-        require( allowance >= defaultStake, "Check the token allowance" );
-        busd.transferFrom(_msgSender(), oracle);
+        require(allowance >= defaultStake, "Check the token allowance");
+        busd.transferFrom(_msgSender(), oracle, defaultStake);
+
+        Player player = Player({
+            timeJoined:     now,
+            amountStaked:   allowance
+        });
+        players[msg.sender] = player;
+
         kawaii.mint(2000 * 10**18); // Mint KAWAII tokens for staking
 
         emit NewUserStakedAccess(msg.sender);
+    }
+
+    ///
+    /// @param x t
+    /// @dev 
+    function withdrawStake() {
+        Player memory player = player[msg.sender];
+        require(player.timeJoined >= player.timeJoined + minStakeDays, "Cannot withdraw yet");
+        busd.transfer(msg.sender, player.amountStake);
+
+        emit UserWithdrewStake(msg.sender);
     }
 
     function updateDefaultStake(uint _defaultStake) public onlyOwner {
